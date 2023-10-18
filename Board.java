88ba5f928219d77ef8e3f8874e49559549240239
Harvey Lowden <pa20854@bristol.ac.uk>
2023-10-18 16:52:09 +0100

    Board displays with tiles now

--- Board.java
+++ Board.java
@@ -2,14 +2,13 @@ import java.util.Arrays;
 
 public class Board {
     
-    //Tile[][] matrix = new Tile[5][5];
-    String[][] matrix = new String[5][5];
+    Tile[][] matrix = new Tile[5][5];
 
     Board(){
         for (int i = 0; i < this.matrix.length; i++) {
             for (int j = 0; j < this.matrix[i].length; j++) {
                 //this.matrix[i][j] = new Tile();
-                this.matrix[i][j] = "cell";
+                this.matrix[i][j] = new Tile();
             }
         }
     }
--- Tile.java
+++ Tile.java
@@ -1,11 +1,18 @@
+import java.util.Arrays;
+
 public class Tile {
     
     boolean state;
-    /*
     Animal animal;
-    */
+    
 
     public Tile(){
         state = false;
+        animal = new Animal("None");
     }
+
+    @Override
+    public String toString() {
+        return "(" + this.state + ", " + this.animal.name + ")";
+    } 
 }
